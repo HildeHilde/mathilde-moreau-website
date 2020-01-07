7fe74026049a31fb66fc37df344bda81e64e0a70
Mathilde Moreau <mathilde.moro@gmail.com>
2020-01-07 16:01:51 +0100

    add resumes and change navbar

---
+++ README.md
@@ -0,0 +1,6 @@
+# mathilde-moreau-site
+
+PROJECT 1 - INTERFACE3
+
+Create a profesionnal website using HTML/CSS, SCSS and JavaScript
+
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import $ from 'jquery';
+
+const newsec = $('#education');
+const newsec2 = $('#work');
+
+
+// $('.link-to-json').click(() => {
+const requestEducation = fetch('../assets/datas/education.json')
+  .then((response) => response.json())
+  .then((data) => {
+    $('#about').after(newsec);
+    $.each(data.education, (i, study) => {
+      $('.package').append(`<h3>${study.where}</h3> <p class="post-title">${study.when}</p><ul class="item"></ul>`);
+      $.each(study.what, (item) => {
+        $('.item').append(`<li class="el">${study.what[item]}</li>`);
+      });
+    });
+  });
+
+const requestWork = fetch('../assets/datas/works.json')
+  .then((response2) => response2.json())
+  .then((data2) => {
+    newsec.after(newsec2);
+    $.each(data2.works, (i, work) => {
+      $('.package2').append(`<h3>${work.title}</h3> <p class="post-title">${work.location} | ${work.date}</p><ul class="item2"></ul>`);
+      $.each(work.descriptions, (element) => {
+        $('.item2').append(`<li class="el">${work.descriptions[element]}</li>`);
+      });
+    });
+  });
+
+
+$.when(requestEducation, requestWork).then(() => {
+  $('#education').show();
+  $('#work').show();
+  $('.link-to-json').hide();
+});
+// });
